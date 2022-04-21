b3812df1d2f2cf5941b339f776f71777ca899da6
Jayson9047 <56447811+Jayson9047@users.noreply.github.com>
2022-04-21 15:18:36 -0400

    Added Third Person Controller
    The Player can now walk, run, jump and the camera will follow

--- TheRevengeOfCreed/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ TheRevengeOfCreed/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -16,10 +16,10 @@ namespace StarterAssets
     {
         [Header("Player")]
         [Tooltip("Move speed of the character in m/s")]
-        public float MoveSpeed = 2.0f;
+        public float MoveSpeed = 5.0f;
 
         [Tooltip("Sprint speed of the character in m/s")]
-        public float SprintSpeed = 5.335f;
+        public float SprintSpeed = 9.0f;
 
         [Tooltip("How fast the character turns to face movement direction")]
         [Range(0.0f, 0.3f)]
